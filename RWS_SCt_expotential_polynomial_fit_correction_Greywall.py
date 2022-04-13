17fe2afd260eaff08c1ddca6a8f3d49bbd6d67e2
timohyva <hyvarinentimo00@gmail.com>
2022-04-13 10:11:12 +0300

    debug of RWS_SCt_expotential_polynomial_fit_correction_Greywall

--- RWS_SCt_expotential_polynomial_fit_correction_Greywall.py
+++ RWS_SCt_expotential_polynomial_fit_correction_Greywall.py
@@ -61,41 +61,59 @@ print("\n q_arr looks like\n", q_arr)
 #############################################################
 ###                  fiting the q_arr                   #####
 #############################################################
+def fit_q_fn2(x, c0, cn1, cn2):
+    delta_p = x - p_pcp_Greywall
+    return cn2*(delta_p**(-2)) + cn1*(delta_p**(-1)) + c0
+
+def fit_q_f0x2(x, c0, c01, c02, c03, c04, c05):
+    delta_p = x - p_pcp_Greywall
+    return c05*(delta_p**(0.5)) + c04*(delta_p**(0.4)) + c03*(delta_p**(0.3)) + c02*(delta_p**(0.2)) + c01*(delta_p**(0.1)) + c0
+
+def fit_q_f0x1(x, c0, c03, c05, c07, c09):
+    delta_p = x - p_pcp_Greywall
+    return c09*(delta_p**(0.9)) + c07*(delta_p**(0.7)) + c05*(delta_p**(0.5)) + c03*(delta_p**(0.3)) + c0
+
 def fit_q_f2(x, c0, c1, c2):
     delta_p = x - p_pcp_Greywall
     return c2*(delta_p**2) + c1*(delta_p) + c0
 
 def fit_q_f3(x, c0, c1, c2, c3):
     delta_p = x - p_pcp_Greywall
-    return c3**(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
+    return c3*(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
  
 def fit_q_f4(x, c0, c1, c2, c3, c4):
     delta_p = x - p_pcp_Greywall
-    return c4*(delta_p**4) + c3**(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
+    return c4*(delta_p**4) + c3*(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
 
 def fit_q_f5(x, c0, c1, c2, c3, c4, c5):
     delta_p = x - p_pcp_Greywall
-    return c5*(delta_p**5) + c4*(delta_p**4) + c3**(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
+    return c5*(delta_p**5) + c4*(delta_p**4) + c3*(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
 
 def fit_q_f6(x, c0, c1, c2, c3, c4, c5, c6):
     delta_p = x - p_pcp_Greywall
-    return c6*(delta_p**6) + c5*(delta_p**5) + c4*(delta_p**4) + c3**(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
+    return c6*(delta_p**6) + c5*(delta_p**5) + c4*(delta_p**4) + c3*(delta_p**3) + c2*(delta_p**2) + c1*(delta_p) + c0
 
 
+poptn2, pcovn2 = cfit(fit_q_fn2, pressure, q_arr)
+popt0x2, pcov0x2 = cfit(fit_q_f0x2, pressure, q_arr)
+popt0x1, pcov0x1 = cfit(fit_q_f0x1, pressure, q_arr)
 popt2, pcov2 = cfit(fit_q_f2, pressure, q_arr)
 popt3, pcov3 = cfit(fit_q_f3, pressure, q_arr)
 popt4, pcov4 = cfit(fit_q_f4, pressure, q_arr)
 popt5, pcov5 = cfit(fit_q_f5, pressure, q_arr)
 popt6, pcov6 = cfit(fit_q_f6, pressure, q_arr)
 
-print("\npopt2 looks like ",popt2,"\npopt3 looks like ",popt3,"\npopt4 looks like ",popt4,"\npopt5 looks like ",popt5,"\npopt6 looks like ",popt6)
+print("\npopt0x2 looks like ",popt0x2,"\npoptn2 looks like ",poptn2,"\npopt2 looks like ",popt2,"\npopt3 looks like ",popt3,"\npopt4 looks like ",popt4,"\npopt5 looks like ",popt5,"\npopt6 looks like ",popt6)
 
 
 #############################################################
 
 fig, ax = plt.subplots(1,1)
 
-#qfit2 = ax.plot(p_arr, fit_q_f2(p_arr, *popt2), 'b-.')
+qfit0x2 = ax.plot(p_arr, fit_q_f0x2(p_arr, *popt0x2), 'k-.', label = r'$q_0.x2 = {0:.6f} + {1:.6f}\Delta p^0.1 + {1:.6f}\Delta p^0.2 + {1:.6f}\Delta p^0.3 + {1:.6f}\Delta p^0.4 + {1:.6f}\Delta p^0.5$'.format(*popt0x2), linewidth = 4)
+# qfitn2 = ax.plot(p_arr, fit_q_fn2(p_arr, *poptn2), 'b-.', label = r'$q_-2 = {0:.6f} + {1:.6f}\Delta p^-1 + {2:.6f}\Delta p^-2$'.format(*poptn2), linewidth = 4)
+qfit0x1 = ax.plot(p_arr, fit_q_f0x1(p_arr, *popt0x1), 'b-.', label = r'$q_0.x = {0:.6f} + {1:.6f}\Delta p^0.3 + {1:.6f}\Delta p^0.5 + {1:.6f}\Delta p^0.7 + {1:.6f}\Delta p^0.9$'.format(*popt0x1), linewidth = 4)
+qfit2 = ax.plot(p_arr, fit_q_f2(p_arr, *popt2), 'g-.', label = r'$q_2 = {0:.6f} + {1:.6f}\Delta p + {2:.6f}\Delta p^2$'.format(*popt2), linewidth = 4)
 #qfit3 = ax.plot(p_arr, fit_q_f3(p_arr, *popt3), 'r-.')
 qfit4 = ax.plot(p_arr, fit_q_f4(p_arr, *popt4), 'r:', label = r'$q_4 = {0:.6f} + {1:.6f}\Delta p + {2:.6f}\Delta p^2 + {3:.6f}\Delta p^3 + {4:.6}\Delta p^4$'.format(*popt4), linewidth = 4)
 qfit5 = ax.plot(p_arr, fit_q_f5(p_arr, *popt5), 'm-', label = r'$q_5 = {0:.6f} + {1:.6f}\Delta p + {2:.6f}\Delta p^2 + {3:.6f}\Delta p^3 + {4:.6}\Delta p^4$ + {5:.6}\Delta p^5'.format(*popt5), linewidth = 4)
@@ -107,6 +125,9 @@ ax.set_xlabel(r"$p/bar$", fontsize = 16)
 ax.set_ylabel(r"$q$", fontsize = 16)
 ax.grid(True)
 ax.legend(fontsize = 13.0)
+ax.tick_params(axis = 'both', which = 'major', labelsize = 24)
+
+# ax.set_ylim([-0.05, 0.1])
 
 # leg = ax.legend([qfit4],[r'$f4 = {0} + {1}\Delta p + {2}\Delta p^2 + {3}\Delta p^3 + {4}\Delta p^4$'.format(*popt4)], fontsize=15.0, loc='lower right')
 
